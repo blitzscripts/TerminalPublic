ec89d922e7c6968d818c5f37d0583add0d1a8c85
Zephrym <Zephrym@users.noreply.github.com>
2018-03-08 01:15:25 -0600

    Credit update

--- Scripts/innerPot.py
+++ Scripts/innerPot.py
@@ -11,13 +11,16 @@ from Quest import *
 # q = quest
 # s = state of the quest, 0 = dont have in log, 1 = have in log, 2 = done or ready to be turned in
 
+
+# inner pot taken from https://www.gamekiller.net/threads/auto-inner-quests.3260592/
+
 def innerPot1():
     q = 12394
     s = GetQuestState(q)
     n = 9010000
     c = s != 2
     if c:
-        StartQuest(q, n)
+        StartQuest(q, n)`
 
 def innerPot2():
     q = 12395
@@ -44,3 +47,4 @@ if level > 100():
     getInnerPot()
 else:
     print("We are not over level 100")
+
