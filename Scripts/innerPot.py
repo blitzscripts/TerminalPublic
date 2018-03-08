b4045550d96c89562aaf88c33f18f1c590767c97
Zephrym <Zephrym@users.noreply.github.com>
2018-03-08 01:18:28 -0600

    white space deletion on innerPot.py

--- Scripts/innerPot.py
+++ Scripts/innerPot.py
@@ -2,7 +2,6 @@ from Character import *
 from Quest import *
 #Some sort of inner ability script
 #Zephryms cleaned up Inner Pot defs
-
 # Local Var Keywords
 # c = condition
 # d = delay
@@ -10,10 +9,7 @@ from Quest import *
 # n = npc
 # q = quest
 # s = state of the quest, 0 = dont have in log, 1 = have in log, 2 = done or ready to be turned in
-
-
 # inner pot taken from https://www.gamekiller.net/threads/auto-inner-quests.3260592/
-
 def innerPot1():
     q = 12394
     s = GetQuestState(q)
@@ -21,7 +17,6 @@ def innerPot1():
     c = s != 2
     if c:
         StartQuest(q, n)`
-
 def innerPot2():
     q = 12395
     s = GetQuestState(q)
@@ -29,7 +24,6 @@ def innerPot2():
     c = s != 2
     if c:
         StartQuest(q, n)
-
 def innerPot3():
     q = 12396
     s = GetQuestState(q)
@@ -37,14 +31,11 @@ def innerPot3():
     c = s != 2
     if c:
         StartQuest(q, n)
-
 def getInnerPot():
     innerPot1()
     innerPot2()
     innerPot3()
-
 if level > 100():
     getInnerPot()
 else:
     print("We are not over level 100")
-
